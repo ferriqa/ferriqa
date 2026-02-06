5c659a69b4a4135b1dfd55f2fff00e3fd1e33f2b
Tufan Tunç <tufan.tunc.91@gmail.com>
2026-02-06 19:00:39 +0300

    style: code format

--- apps/admin-ui/src/lib/i18n.ts
+++ apps/admin-ui/src/lib/i18n.ts
@@ -1,15 +1,18 @@
-import * as runtime from '$lib/paraglide/runtime.js';
+import * as runtime from "$lib/paraglide/runtime.js";
 
 export const i18n = {
-  get locale() { return runtime.getLocale(); },
+  get locale() {
+    return runtime.getLocale();
+  },
   locales: runtime.locales,
   getLocale: runtime.getLocale,
   setLocale: runtime.setLocale,
   resolveRoute: (path: string, lang: string) => {
-    const url = new URL(path, 'http://localhost');
+    const url = new URL(path, "http://localhost");
     return runtime.localizeUrl(url, { locale: lang }).pathname;
   },
   route: (pathname: string) => {
-    return runtime.deLocalizeUrl(new URL(pathname, 'http://localhost')).pathname;
-  }
+    return runtime.deLocalizeUrl(new URL(pathname, "http://localhost"))
+      .pathname;
+  },
 };
